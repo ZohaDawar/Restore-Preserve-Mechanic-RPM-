e6c61df3fdc48f090611b73cc4ffab5836ea8027
ZohaDawar <zoha.dawar@gmail.com>
2026-03-17 22:57:20 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+from routes.products import products_bp
+from routes.cart import cart_bp
+from routes.orders import orders_bp
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(products_bp, url_prefix="/products")
+app.register_blueprint(cart_bp, url_prefix="/cart")
+app.register_blueprint(orders_bp, url_prefix="/orders")
+
+if __name__ == "__main__":
+    app.run(debug=True)
