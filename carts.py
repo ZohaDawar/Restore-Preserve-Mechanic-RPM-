8750b3a6015886e7a06bb89a9e300220ca37634d
khanraahim2005-glitch <khanraahim2005@gmail.com>
2026-03-18 20:26:26 +0500

    carts push

---
+++ carts.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, jsonify, request
+from db import get_connection
+
+cart_bp = Blueprint("cart", __name__)
+
+@cart_bp.route("/", methods=["GET"])
+def view_cart():
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    SELECT P.PRODUCT_NAME, C.QUANTITY, P.PRICE_PKR
+    FROM CART C
+    JOIN PRODUCT P
+    ON C.PRODUCT_ID = P.PRODUCT_ID
+    WHERE C.STATUS='active'
+    """
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return jsonify(rows)
+
+
+@cart_bp.route("/add", methods=["POST"])
+def add_to_cart():
+
+    data = request.json
+    product_id = data["product_id"]
+    quantity = data["quantity"]
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO CART (CART_ID, PRODUCT_ID, QUANTITY, ADDED_DATE, STATUS)
+    VALUES (CART_SEQ.NEXTVAL, :pid, :qty, SYSDATE, 'active')
+    """
+
+    cursor.execute(query, {"pid": product_id, "qty": quantity})
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return jsonify({"message": "Product added to cart"})
